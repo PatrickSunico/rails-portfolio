449f748da33f53a1b5cbb996a28ea6ee0add1241
PatrickSunico <apatricksunico@gmail.com>
2016-08-04 23:14:51 +0800

    Minor Fixes

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,2 +1,32 @@
+// sticky Nav Animation
 
-$(window).scroll(function(){$(this).scrollTop()>1?$("nav").addClass("sticky"):$("nav").removeClass("sticky")}),$(window).scroll(function(){$(".navbar").offset().top>50?$(".navbar-fixed-top").addClass("top-nav-collapse"):$(".navbar-fixed-top").removeClass("top-nav-collapse")}),$(function(){$("a.page-scroll").bind("click",function(o){var a=$(this);$("html, body").stop().animate({scrollTop:$(a.attr("href")).offset().top},1500,"easeInOutExpo"),o.preventDefault()})});
+$(window).scroll(function() {
+
+    if($(this).scrollTop() > 1){
+        $('nav').addClass("sticky")
+    }
+    else{
+
+        $('nav').removeClass("sticky");
+    }
+});
+
+//jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
