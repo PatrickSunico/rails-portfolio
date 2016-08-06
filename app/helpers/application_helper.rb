0f15ecd01fc9410b910a278294afbb9419f559d3
PatrickSunico <apatricksunico@gmail.com>
2016-08-06 17:26:22 +0800

    Nested Partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
+  # Notices
   def flash_alerts
     render(:partial => "shared/notice")
+  end
+
+  # Portfolio Section Structured partial
+  def section_for(title, &block)
+    render(:partial => 'editable-sections/portfolio-section', :locals => {:title => title, :block => block})
   end
 end
