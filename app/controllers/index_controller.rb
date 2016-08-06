18a8a81519773b0e376be0238451d5c341b14c6b
PatrickSunico <apatricksunico@gmail.com>
2016-08-06 23:50:58 +0800

    Fixed Json as static store

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,6 +1,15 @@
+require 'json'
 class IndexController < ApplicationController
   layout 'portfolio-layout'
-  def index
-    # services = ServivesSection.all
+    def index
+        @data = JSON.parse(File.read("#{Rails.root}/app/assets/resources/data.json")) do
+          respond_to do |format|
+            format.html
+            format.json { render json: @data }
+        end
+
+
+    end
+
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,18 @@ module ApplicationHelper
   def flash_alerts
     render(:partial => "shared/notice")
   end
+  # About Section
+  def about_section(title, &block)
+
+  end
+
+  # Services Section
+  def services_section(title, &block)
+    render(:partial => 'editable-sections/services-section', :locals => {:title => title, :block => block})
+  end
 
   # Portfolio Section Structured partial
-  def section_for(title, &block)
+  def portfolio_section(title, &block)
     render(:partial => 'editable-sections/portfolio-section', :locals => {:title => title, :block => block})
-
   end
 end
