4e5d2bd8685c449874ad1b09e6fce1b4fcd9b9a5
PatrickSunico <apatricksunico@gmail.com>
2016-08-07 16:17:04 +0800

    JSON Static Store fixed

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,14 +2,17 @@ require 'json'
 class IndexController < ApplicationController
   layout 'portfolio-layout'
     def index
-        @data = JSON.parse(File.read("#{Rails.root}/app/assets/resources/data.json")) do
-          respond_to do |format|
-            format.html
-            format.json { render json: @data }
+            @data = JSON.parse(File.read("#{Rails.root}/app/assets/resources/data.json")) do
+              respond_to do |format|
+                format.html
+                format.json { render json: @data }
+            end
+          end
+            @services = JSON.parse(File.read("#{Rails.root}/app/assets/resources/services.json")) do
+              respond_to do |format|
+                format.html
+                format.json { render json: @services }
+            end
         end
-
-
     end
-
-  end
 end
