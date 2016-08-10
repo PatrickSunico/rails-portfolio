9876f7911346ca93a7dd84586c5f54d0bcb73020
PatrickSunico <apatricksunico@gmail.com>
2016-08-10 19:39:37 +0800

    rename

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,5 @@
 module ApplicationHelper
-  # Notices
-  def flash_alerts
-    render(:partial => "shared/notice")
-  end
   # About Section
-  def about_section(title, &block)
-
-  end
-
   # Services Section
   def services_section(title, &block)
     render(:partial => 'editable-sections/services-section', :locals => {:title => title, :block => block})
