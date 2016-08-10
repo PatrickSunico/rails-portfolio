d83a010c29523b1e2901fd681cddf553f131f979
PatrickSunico <apatricksunico@gmail.com>
2016-08-10 15:46:11 +0800

    Production Test

--- app/assets/javascripts/contacts.coffee
+++ app/assets/javascripts/contacts.coffee
@@ -1,13 +1,3 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-
-# $(document).ready ->
-#   $("#new_contact").bind("ajax:success", (e, data, status, xhr) ->
-#     # console.log(e)
-#     e.preventDefault()
-#     console.log(status)
-#     console.log(xhr.responseText)
-#     $(".alert").append("Success")
-#   ).bind "ajax:error", (e, xhr, status, error) ->
-#     $(".alert").append("Error")
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,27 +1,31 @@
 class ContactsController < ApplicationController
   layout "portfolio-layout"
+
   def new
     # Render the contact view inside of index
     @contact = Contact.new
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @contact }
-    end
+    # respond_to do |format|
+    #   format.html
+    #   format.json { render json: @contact }
+    # end
   end
 
   def create
     # Pass the data from form to /create
     @contact = Contact.new(contact_params)
     @contact.request = request
-    if @contact.deliver
+    # if result = success
+    # Default
+    if @result = @contact.deliver
+
+    # if @contact.deliver
       # Default Mail_form
       # flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
       # render "new"
       # # #
-
       respond_to do |format|
-        format.html { render :new, notice: "Post Successful" }
-        format.json
+        # format.html { render :new, notice: "Post Successful" }
+        format.json { render json: @contact, status: :created, location: @contact }
         format.js
       end
     else
@@ -30,11 +34,23 @@ class ContactsController < ApplicationController
       # render :new
 
       respond_to do |format|
-        format.html { render :new , notice: "Post Error"}
+        # format.html { render :new , notice: "Post Error"}
+        format.json { render json: @contact.errors, status: :unprocessable_entity  }
         format.js
       end
-    end
-  end
+
+      # respond_to do |format|
+      #   format.html do
+      #     if request.xhr?
+      #       render :json => @contact.errors, :status => :unprocessable_entity
+      #     else
+      #       render :new, :status => :unprocessable_entity
+      #     end
+      #   end
+      # end
+
+    end # end if/else
+  end # End create
 
   private
     def contact_params
