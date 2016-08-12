6cb003ddf51ceeb46384df353692eda8aff75af4
PatrickSunico <apatricksunico@gmail.com>
2016-08-12 21:42:10 +0800

    Fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,21 @@
 module ApplicationHelper
-  # About Section
   # Services Section
   def services_section(title, &block)
     render(:partial => 'editable-sections/services-section', :locals => {:title => title, :block => block})
   end
 
+  # Toolset Section
+  def toolset_section(title, &block)
+    render(:partial => 'editable-sections/toolset-section', :locals => {:title => title, :block => block})
+  end
+
+
+
   # Portfolio Section Structured partial
   def portfolio_section(title, &block)
     render(:partial => 'editable-sections/portfolio-section', :locals => {:title => title, :block => block})
   end
+
   # Contact Section Structured partial
   # Render a structured partial then inside of the structure partial render the form
   def contact_section(title, &block)
