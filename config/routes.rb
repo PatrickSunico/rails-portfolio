a22789139b7de87dd6b0a102d628c4fe2cf98ea1
PatrickSunico <apatricksunico@gmail.com>
2016-08-14 15:52:44 +0800

    Added Google Analytics

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'robots.:format' => 'robots#index'
   root 'index#index'
   get "index/download"
   post "contacts" => "contacts#create"
   get '*path' => redirect('/')
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
